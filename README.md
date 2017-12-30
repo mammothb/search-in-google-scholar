bfb11cc0403c4e5665fdc62749c436d4a8647d48
mmb <mammothb+git@gmail.com>
2017-12-30 11:50:53 +0800

    Prepare for v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# search-in-google-scholar
+# Search in Google Scholar
+
+Search selected text in Google Scholar
+
+## Demo
+![demo](https://github.com/mammothb/search-in-google-scholar/raw/master/images/demo.png)
+
+## Acknowledgement
+* Icons made by [Smashicons](https://www.flaticon.com/authors/smashicons) from [Flaticon](https://www.flaticon.com/) is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/)
+* Legacy extension created by [Vincent Piras](https://sites.google.com/site/vincentpiras/sigs)
---
+++ background.js
@@ -0,0 +1,28 @@
+browser.contextMenus.create({
+  id: "search-in-google-scholar",
+  title: "Search in Google Scholar",
+  contexts: ["selection"],
+});
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "search-in-google-scholar") {
+    // Encodes the selected text as a URI component
+    const queryText = encodeFormDataURIComponent(info.selectionText);
+    // Construct the query URL
+    const queryUrl = "https://scholar.google.com/scholar?q=" + queryText;
+
+    // Create a new tab under the current tab, does not switch to it
+    // immediately
+    browser.tabs.create({
+      active: false,
+      openerTabId: tab.id,
+      url: queryUrl
+    }).catch((error) => {
+      console.error("Failed to open new tab: " + error);
+    });
+  }
+});
+
+function encodeFormDataURIComponent(str) {
+  // Replace spaces with + for application/x-www-form-urlencoded
+  return encodeURIComponent(str).replace(/%20/g, "+");
+}
